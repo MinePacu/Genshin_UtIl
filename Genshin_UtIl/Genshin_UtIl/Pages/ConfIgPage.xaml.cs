793689edbc0134df5cd70171e9b483737af3ef63
Pacu <subin11@hotmail.co.kr>
2023-02-21 12:07:39 +0900

    Config 페이지 MVVM 구조로 변경
    - ConfigPage.xaml, ConfigPage.xaml.cs -> MVVM 구조로 변경함에 따른 코드 정리
    - ConfigViewModel.cs -> MVVM 구조에 따른 뷰모델 추가

--- Genshin_UtIl/Genshin_UtIl/Pages/ConfIgPage.xaml.cs
+++ Genshin_UtIl/Genshin_UtIl/Pages/ConfIgPage.xaml.cs
@@ -17,28 +17,9 @@ namespace Genshin_UtIl.Pages
     /// </summary>
     public sealed partial class ConfIgPage : Page
     {
-        public int AppColor { get; set; }
-
-        public bool IsUseDeveloperUtil { get; set; }
-
-        public int FirstPage { get; set; }
-
         public ConfIgPage()
         {
             this.InitializeComponent();
-
-            AppColor = ConfIg.Instance.AppTh;
-
-            IsUseDeveloperUtil = ConfIg.Instance.Dev;
-
-            if (ConfIg.Instance.FIrstPage == "WIndow")
-                FirstPage = 0;
-
-            else if (ConfIg.Instance.FIrstPage == "ReShadeCon")
-                FirstPage = 1;
-
-            else if(ConfIg.Instance.FIrstPage == "OpenGameCon")
-                FirstPage = 2;
         }
 
         void AppThChanged(object sender, SelectionChangedEventArgs e)
@@ -69,33 +50,6 @@ namespace Genshin_UtIl.Pages
                 WIndowUtIl.N.RequestedTheme = ElementTheme.Dark;
                 DwmUtil.DwmSetWindowAttribute_(WIndowUtIl.Hwnd, UtIls.Dwm.Enum.DwmWIndowAttrIbute.DWMWA_USE_IMMERSIVE_DARK_MODE, true);
             }
-
-            ConfIg.Instance.AppTh = Appc.SelectedIndex;
-        }
-
-        void UtIlChanged(object sender, RoutedEventArgs e)
-        {
-            var tmp = (ToggleSwitch) sender;
-
-            if (tmp.IsOn == false)
-                ConfIg.Instance.Dev = false;
-
-            else if (tmp.IsOn == true)
-                ConfIg.Instance.Dev = true;
-        }
-
-        void PageConChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var PageCon = (ComboBox)sender;
-
-            if (PageCon.SelectedIndex == 0)
-                ConfIg.Instance.FIrstPage = "WIndow";
-
-            else if (PageCon.SelectedIndex == 1)
-                ConfIg.Instance.FIrstPage = "ReShadeCon";
-
-            else if (PageCon.SelectedIndex == 2)
-                ConfIg.Instance.FIrstPage = "OpenGameCon";
         }
     }
 }
---
+++ Genshin_UtIl/Genshin_UtIl/ViewModels/ConfigViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using Genshin_UtIl.UtIls;
+
+namespace Genshin_UtIl.ViewModels
+{
+    public partial class ConfigViewModel : ObservableObject
+    {
+        private int appColor;
+
+        public int AppColor
+        {
+            get => appColor;
+            set
+            {
+                SetProperty(ref appColor, value);
+                ConfIg.Instance.AppTh = value;
+            }
+        }
+
+        private bool isUseDeveloperUtil;
+
+        public bool IsUseDeveloperUtil
+        {
+            get => isUseDeveloperUtil;
+            set
+            {
+                SetProperty(ref isUseDeveloperUtil, value);
+                ConfIg.Instance.Dev = value;
+            }
+        }
+
+        private int firstPage;
+
+        public int FirstPage
+        {
+            get => firstPage;
+            set
+            {
+                SetProperty(ref firstPage, value);
+                if (value == 0)
+                    ConfIg.Instance.FIrstPage = "WIndow";
+
+                else if (value == 1)
+                    ConfIg.Instance.FIrstPage = "ReShadeCon";
+
+                else if (value == 2)
+                    ConfIg.Instance.FIrstPage = "OpenGameCon";
+            }
+        }
+
+        public ConfigViewModel()
+        {
+            AppColor = ConfIg.Instance.AppTh;
+            IsUseDeveloperUtil = ConfIg.Instance.Dev;
+
+            if (ConfIg.Instance.FIrstPage == "WIndow")
+                FirstPage = 0;
+
+            else if (ConfIg.Instance.FIrstPage == "ReShadeCon")
+                FirstPage = 1;
+
+            else if (ConfIg.Instance.FIrstPage == "OpenGameCon")
+                FirstPage = 2;
+        }
+    }
+}
